f22b03701c8ab9cbaffe5e86438fde83431d9f63
Peter Lau <superpeterlau@outlook.com>
2022-06-07 03:39:50 +0800

    auth init

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
 // We import the CSS which is extracted to its own file by esbuild.
 // Remove this line if you add a your own CSS build pipeline (e.g postcss).
 
-// import "../css/app.css";
+import "../css/app.css";
 import "../css/tailwind.css";
 
 // Fontawesome
